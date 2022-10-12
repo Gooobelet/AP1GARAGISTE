57edf037b30f062543faec01f08a24ef5da89cef
Miguel <miguel.fenerol@gmail.com>
2022-10-12 11:59:42 +0200

    <dev>(v3.6)

---
+++ src/Entity/PRESTATION.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PRESTATIONRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PRESTATIONRepository::class)
+ */
+class PRESTATION
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $img;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $temps;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function setImg(string $img): self
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getTemps(): ?string
+    {
+        return $this->temps;
+    }
+
+    public function setTemps(string $temps): self
+    {
+        $this->temps = $temps;
+
+        return $this;
+    }
+}
